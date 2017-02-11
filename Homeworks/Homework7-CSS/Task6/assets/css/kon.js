4b89e855be6ae43061365ff768d8a1d892486ccb
Denislav Enchev <deni.b.enchev@gmail.com>
2017-02-11 21:44:49 +0200

    Task 6 - Recreate a page with moved main side away from footer

---
+++ Homeworks/Homework7-CSS/Task6/assets/css/kon.js
@@ -0,0 +1,7 @@
+var boi = ["Спатия", "Каро", "Купа", "Пика"];
+var nomer = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+for (var i = 0; i < boi.length; i++) {
+    for (var j = 0; j < nomer.length; j++) {
+        console.log(nomer[j] + boi[i]);
+    }
+}
